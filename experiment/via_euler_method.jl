fdc8389dcf5e83efad3bdd552d3606f0feb247e8
wmkouw <wmkouw@gmail.com>
2020-05-11 17:01:09 +0200

    Started working with LAR module

--- experiment/via_euler_method.jl
+++ experiment/via_euler_method.jl
@@ -17,7 +17,7 @@ T = size(df, 1)
 """
 State-space formulation of Silverbox's dynamics:
 
-μ⋅d^2/dt^2 x(t) + ν⋅d/dt x(t) + κ(t)⋅x(t) = u(t) + w(t)
+μ⋅d^2/dt^2 x(t) + ν⋅d/dt x(t) + κ(x(t))⋅x(t) = u(t) + w(t)
 κ(x(t)) = α + β x^2(t)
 y(t) = x(t) + e(t)
 
@@ -35,7 +35,7 @@ I now take a series of steps to re-write this problem:
 
 1. Assume constant spring coefficient κ
 
-μ⋅ d^2/dt^2 x(t) + ν⋅d/dt x(t) + κ⋅ x(t) = u(t) + w(t)
+μ⋅ d^2/dt^2 x(t) + ν⋅d/dt x(t) + κ⋅x(t) = u(t) + w(t)
 
 2. Divide by leading coefficient
 
@@ -43,7 +43,7 @@ d^2/dt^2 x(t) + (ν/μ)⋅d/dt x(t) + (κ/μ)⋅ x(t) - u(t)/μ = w(t)/μ
 
 3. Substitute variables (natural frequency ω_0 and damping ratio ζ)
 
-d^2/dt^2 x(t) + 2ζω_0⋅d/dt x(t) + ω_0^2⋅ x(t) - u(t)/μ = w(t)/μ
+d^2/dt^2 x(t) + 2ζω_0⋅d/dt x(t) + ω_0^2⋅x(t) - u(t)/μ = w(t)/μ
 
 4. Apply Euler's method to obtain difference equation (step size is 1)
 
@@ -69,21 +69,24 @@ d^2/dt^2 x(t) + 2ζω_0⋅d/dt x(t) + ω_0^2⋅ x(t) - u(t)/μ = w(t)/μ
 
     z_t = M⋅z_t-1 + N⋅u_t + N⋅w_t
 
-    where z_t = [x_t, x_t-1], M = [α β; 1 0], N = [γ 0]
+    where z_t = [x_t x_t-1],
+            M = [α β;
+                 1 0],
+            N = [γ 0]
 
 5. Convert to Gaussian probability
 
 - Backward Euler:
 
-    z_t ~ N(M⋅z_t-1 + N⋅u_t, N⋅τ)
+    z_t ~ Normal(M⋅z_t-1 + N⋅u_t, N⋅τ)
 
-    where w_t ~ N(0, τ)
+    where w_t ~ Normal(0, τ)
 
 7. Observation likelihood
 
-    y_t ~ N(H⋅z_t, σ)
+    y_t ~ Normal(H⋅z_t, σ)
 
-    where e_t ~ N(0, σ), c = [1 0]
+    where e_t ~ Normal(0, σ), c = [1 0]
 
 Now, I need priors for α, β, γ, τ, σ. Given three equations and three unknowns,
 I can recover ζ, ω_0 and μ from α, β, and γ. The variables are all strictly
@@ -98,9 +101,10 @@ positive, which means they should be modeled by gamma distributions:
 """
 
 using ForneyLab
-using LAR.AR, LAR.DataAR
-import LAR: logPDF, wmse
+using LAR
+using ProgressMeter
 
+# Start graph
 g = FactorGraph()
 
 # Static parameters
@@ -108,27 +112,80 @@ g = FactorGraph()
 @RV β ~ Gamma(1, 1e3)
 @RV γ ~ Gamma(1, 1e3)
 @RV τ ~ Gamma(1, 1e3)
-@RV σ ~ Gamma(1, 1e3)
 
-# Dynamic parameters
-u = Vector{Variable}(undef, T)
-x = Vector{Variable}(undef, T)
-y = Vector{Variable}(undef, T)
+# I'm fixing measurement noise σ
+σ = 10.
 
-x_tmin1 = x_0
-x_tmin2 = x_0
+# Observation selection variable
+c = [1, 0]
+
+# Convenience variables
+θ = [α β]
+
+# State prior
+@RV z_t ~ GaussianMeanPrecision(placeholder(:m_z_t, dims=(2,)), placeholder(:w_z_t, dims=(2, 2)))
+
+# Autoregressive node
+@RV x_t ~ Autoregressive(θ, z_t, τ)
+
+# Specify likelihood
+@RV y_t ~ GaussianMeanPrecision(dot(c, x_t), σ)
+
+# Placeholder for data
+placeholder(y_t, :y_t)
+
+# Recognition factorization
+q = RecognitionFactorization(x_t, z_t, θ, τ, ids=[:x, :z, :θ, :τ])
+algo = variationalAlgorithm(q)
+eval(Meta.parse(algo))
+
+# Inference parameters
+num_iterations = 10
+
+# Initialize marginal distribution and observed data dictionaries
+data = Dict()
+marginals = Dict()
+
+# Initialize arrays of parameterizations
+params_x = zeros(T+1,2)
+params_z = zeros(T+1,2)
+params_θ = zeros(T+1,2)
+params_τ = zeros(T+1,2)
+
+# Start progress bar
+p = Progress(T, 1, "At time ")
+
+# Perform inference at each time-step
 for t = 1:T
 
-    # Specify state transition
-    @RV x[t] ~ GaussianMeanVariance((2*μ + ν)/(μ + ν + κ[t])*x_tmin1 - μ/(μ + ν + κ[t])*x_tmin2 + u[t], σ_w)
+    # Update progress bar
+    update!(p, t)
 
-    # Specify likelihood
-    @RV y[t] ~ GaussianMeanVariance(x[t], σ_e)
+    # Initialize marginals
+    marginals[:x] = ProbabilityDistribution(Multivariate, GaussianMeanPrecision, m=params_x[t,1], w=params_x[t,2])
+    marginals[:z] = ProbabilityDistribution(Multivariate, GaussianMeanPrecision, m=params_z[t,1], w=params_z[t,2])
+    marginals[:θ] = ProbabilityDistribution(Multivariate, GaussianMeanPrecision, m=params_θ[t,1], w=params_θ[t,2])
+    marginals[:τ] = ProbabilityDistribution(Univariate, Gamma, a=params_τ[1], b=params_τ[2])
 
-    # Specfify observed variables
-    placeholder(u[t], :u, index=t)
-    placeholder(y[t], :y, index=t)
+    # Iterate variational parameter updates
+    for i = 1:num_iterations
 
-    x_tmin1 = x[t]
-    x_tmin2 = x_tmin1
+        stepx!(data, marginals)
+        stepz!(data, marginals)
+        stepθ!(data, marginals)
+        stepτ!(data, marginals)
+    end
+
+    # Store current parameterizations of marginals
+    params_x[t+1,1] = unSafeMean(marginals[:x])
+    params_x[t+1,2] = unSafePrecision(marginals[:x])
+    params_z[t+1,1] = unSafeMean(marginals[:z])
+    params_z[t+1,2] = unSafePrecision(marginals[:z])
+    params_θ[t+1,1] = unSafeMean(marginals[:θ])
+    params_θ[t+1,2] = unSafePrecision(marginals[:θ])
+    params_τ[t+1,1] = marginals[:τ].params[:a]
+    params_τ[t+1,2] = marginals[:τ].params[:b]
+
+end
+
 end
