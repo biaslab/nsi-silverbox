69748184c74cf33f7cea016a62c5dc1ec993e4c1
wmkouw <wmkouw@gmail.com>
2020-09-23 11:32:35 +0200

    Finished mean-field message computations

--- GFX-node/gfx_update_rules.jl
+++ GFX-node/gfx_update_rules.jl
@@ -21,11 +21,11 @@ function ruleVariationalGFXOutNPPPPP(Δt :: Float,
                                      marg_τ :: ProbabilityDistribution{Univariate})
 
 	# Extract moments of beliefs
-	mθ = unsafeMean(marg_θ)
-	mx = unsafeMean(marg_x)
-	mη = unsafeMean(marg_η)
-	mu = unsafeMean(marg_u)
-	mτ = unsafeMean(marg_τ)
+	mθ,Vθ = unsafeMeanCov(marg_θ)
+	mx,Vx = unsafeMeanCov(marg_x)
+	mη,Vη = unsafeMeanCov(marg_η)
+	mu,Vu = unsafeMeanCov(marg_u)
+	mτ,Vτ = unsafeMeanCov(marg_τ)
 
 	# Set order
 	order = dims(marg_θ)
@@ -38,7 +38,7 @@ function ruleVariationalGFXOutNPPPPP(Δt :: Float,
 	mW = wMatrix(mτ, order)
 
 	# Compute expected values
-	EA = (S + s*mθ')
+	EA = S + s*mθ'
 	EB = s*mη
 
 	# Set parameters
@@ -50,20 +50,19 @@ function ruleVariationalGFXOutNPPPPP(Δt :: Float,
 end
 
 function ruleVariationalGFXIn1PNPPPP(Δt :: Float,
-									 marg_y :: ProbabilityDistribution{Univariate},
+									 marg_y :: ProbabilityDistribution{Multivariate},
                                      marg_θ :: Nothing,
 									 marg_x :: ProbabilityDistribution{Multivariate},
-                                     marg_z :: ProbabilityDistribution{Multivariate},
+                                     marg_η :: ProbabilityDistribution{Univariate},
                                      marg_u :: ProbabilityDistribution{Univariate},
                                      marg_τ :: ProbabilityDistribution{Univariate})
 
-
 	# Extract moments of beliefs
-	mθ,Vθ = unsafeMeanCov(marg_θ)
+	my,Vy = unsafeMeanCov(marg_y)
 	mx,Vx = unsafeMeanCov(marg_x)
-	mη = unsafeMean(marg_η)
-	mu = unsafeMean(marg_u)
-	mτ = unsafeMean(marg_τ)
+	mη,Vη = unsafeMeanCov(marg_η)
+	mu,Vu = unsafeMeanCov(marg_u)
+	mτ,Vτ = unsafeMeanCov(marg_τ)
 
 	# Set order
 	order = dims(marg_θ)
@@ -75,51 +74,159 @@ function ruleVariationalGFXIn1PNPPPP(Δt :: Float,
 	# Cast precision to matrix
 	mW = wMatrix(mτ, order)
 
+	# Set parameters
+	ϕ = mx*s'*mW*(my - s*mη*mu) - mW*S'*Vx*s
+	Φ = mτ*(mx*mx' + Vx)
 
+	# Set outgoing message
+	return Message(Multivariate, GaussianWeightedMeanPrecision, xi=ϕ, w=Φ)
 end
 
 function ruleVariationalGFXIn2PPNPPP(Δt :: Float,
-									 marg_y :: ProbabilityDistribution{Univariate},
+									 marg_y :: ProbabilityDistribution{Multivariate},
 									 marg_θ :: ProbabilityDistribution{Multivariate},
 									 marg_x :: Nothing,
-							  	     marg_z :: ProbabilityDistribution{Multivariate},
+									 marg_η :: ProbabilityDistribution{Univariate},
 								     marg_u :: ProbabilityDistribution{Univariate},
                                      marg_τ :: ProbabilityDistribution{Univariate})
 
-   #TODO
+   	# Extract moments of beliefs
+	my,Vy = unsafeMeanCov(marg_y)
+	mθ,Vθ = unsafeMeanCov(marg_θ)
+	mη,Vη = unsafeMeanCov(marg_η)
+	mu,Vu = unsafeMeanCov(marg_u)
+	mτ,Vτ = unsafeMeanCov(marg_τ)
+
+	# Set order
+	order = dims(marg_θ)
+
+	# Define helper matrices
+	S = Bidiagonal(ones(order,), Δt.*ones(order-1,), :U)
+	s = zeros(order,); s[end] = 1.
+
+	# Cast precision to matrix
+	mW = wMatrix(mτ, order)
+	
+	# Compute expected values
+	EA = S + s*mθ'
+
+	# Set parameters
+	ϕ = EA*mW*(my - s*mη*mu)
+	Φ = mW*(EA'*EA + Vθ)
+
+	# Set outgoing message
+	return Message(Multivariate, GaussianWeightedMeanPrecision, xi=ϕ, w=Φ)
 end
 
 function ruleVariationalGFXIn3PPPNPP(Δt :: Float,
-									 marg_y :: ProbabilityDistribution{Univariate},
+									 marg_y :: ProbabilityDistribution{Multivariate},
 									 marg_θ :: ProbabilityDistribution{Multivariate},
 									 marg_x :: ProbabilityDistribution{Multivariate},
-							  	     marg_z :: Nothing,
+							  	     marg_η :: Nothing,
 								     marg_u :: ProbabilityDistribution{Univariate},
                                      marg_τ :: ProbabilityDistribution{Univariate})
 
-   #TODO
+	# Extract moments of beliefs
+	my,Vy = unsafeMeanCov(marg_y)
+	mθ,Vθ = unsafeMeanCov(marg_θ)
+	mx,Vx = unsafeMeanCov(marg_x)
+	mu,Vu = unsafeMeanCov(marg_u)
+	mτ,Vτ = unsafeMeanCov(marg_τ)
+
+	# Set order
+	order = dims(marg_θ)
+
+	# Define helper matrices
+	S = Bidiagonal(ones(order,), Δt.*ones(order-1,), :U)
+	s = zeros(order,); s[end] = 1.
+
+	# Cast precision to matrix
+	mW = wMatrix(mτ, order)
+	
+	# Compute expected values
+	EA = S + s*mθ'
+
+	# Set parameters
+	ϕ = mu'*s'*mW*(my - EA*mx)
+	Φ = mτ*(mu*mu' + Vu)
+
+	# Set outgoing message
+	return Message(Univariate, GaussianWeightedMeanPrecision, xi=ϕ, w=Φ)
 end
 
 function ruleVariationalGFXIn4PPPPNP(Δt :: Float,
-									 marg_y :: ProbabilityDistribution{Univariate},
+									 marg_y :: ProbabilityDistribution{Multivariate},
 									 marg_θ :: ProbabilityDistribution{Multivariate},
 									 marg_x :: ProbabilityDistribution{Multivariate},
-							  	     marg_z :: ProbabilityDistribution{Multivariate},
+									 marg_η :: ProbabilityDistribution{Univariate},
 								     marg_u :: Nothing,
                                      marg_τ :: ProbabilityDistribution{Univariate})
 
-   #TODO
+   
+	# Extract moments of beliefs
+	my,Vy = unsafeMeanCov(marg_y)
+	mθ,Vθ = unsafeMeanCov(marg_θ)
+	mx,Vx = unsafeMeanCov(marg_x)
+	mη,Vη = unsafeMeanCov(marg_η)
+	mτ,Vτ = unsafeMeanCov(marg_τ)
+
+	# Set order
+	order = dims(marg_θ)
+
+	# Define helper matrices
+	S = Bidiagonal(ones(order,), Δt.*ones(order-1,), :U)
+	s = zeros(order,); s[end] = 1.
+
+	# Cast precision to matrix
+	mW = wMatrix(mτ, order)
+	
+	# Compute expected values
+	EA = S + s*mθ'
+
+	# Set parameters
+	ϕ = mη'*s'*mW*(my - EA*mx)
+	Φ = mτ*(mη*mη' + Vη)
+
+	# Set outgoing message
+	return Message(Univariate, GaussianWeightedMeanPrecision, xi=ϕ, w=Φ)
 end
 
 function ruleVariationalGFXIn5PPPPPN(Δt :: Float,
-									 marg_y :: ProbabilityDistribution{Univariate},
+									 marg_y :: ProbabilityDistribution{Multivariate},
 									 marg_θ :: ProbabilityDistribution{Multivariate},
 									 marg_x :: ProbabilityDistribution{Multivariate},
-							  	     marg_z :: ProbabilityDistribution{Multivariate},
+									 marg_η :: ProbabilityDistribution{Univariate},
 								     marg_u :: ProbabilityDistribution{Univariate},
                                      marg_τ :: Nothing)
 
-   #TODO
+   
+	# Extract moments of beliefs
+	my,Vy = unsafeMeanCov(marg_y)
+	mθ,Vθ = unsafeMeanCov(marg_θ)
+	mx,Vx = unsafeMeanCov(marg_x)
+	mη,Vη = unsafeMeanCov(marg_η)
+	mu,Vu = unsafeMeanCov(marg_u)
+	
+	# Set order
+	order = dims(marg_θ)
+
+	# Define helper matrices
+	S = Bidiagonal(ones(order,), Δt.*ones(order-1,), :U)
+	s = zeros(order,); s[end] = 1.
+
+	# Cast precision to matrix
+	mW = wMatrix(mτ, order)
+	
+	# Compute expected values
+	EA = S + s*mθ'
+	EB = s*mη
+
+	# Set parameters
+	a = 3/2
+	b = my[order] + Vy[order,order] -2*my[order]*(EA*mx)[order] -2*(EA*mx)[order]*(EB*mu)[order] -2*my[order]*(EB*mu)[order] + mx'*(EA'*EA + Vθ)*mx + tr((EA'*EA + Vθ)*Vx) + (mη^2 + Vη)*(mu^2 + Vu)
+
+	# Set outgoing message
+	return Message(Univariate, Gamma, a=a, b=b)
 end
 
 function collectNaiveVariationalNodeInbounds(node::GeneralisedFilterX, entry::ScheduleEntry)
